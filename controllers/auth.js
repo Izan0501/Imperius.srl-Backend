d44d52338917b9518d92ff53213fd220b2dd4dc4
Izan <ivozanacchi501@gmail.com>
2024-08-27 03:18:18 -0300

    addRegisterFunctionChanges

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,43 @@
-function register(req, res) {
-    console.log('Correctly Executed');
+// req: Recibe client data
+// res: send an answer to client
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
-    res.status(200).send({ msg: 'Conected' });
-    
+async function register(req, res) {
+    const { firstname, lastname, email, password } = req.body;
+
+    if (!email) {
+        res.status(400).send({ msg: 'necessary email' });
+        return;
+    }
+
+    if (!password) {
+        res.status(401).send({ msg: 'necessary password' });
+        return;
+    }
+
+    const user = new User({
+        firstname: firstname,
+        lastname: lastname,
+        email: email.toLowerCase(),
+        password: password,
+        role: 'user',
+        active: true
+    });
+
+    // HIDE PASS
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(password, salt);
+    user.password = hashPassword;
+
+    // SAVE USER
+    try {
+        await user.save();
+        res.status(200).send({ msg: 'saved user' })
+    } catch (error) {
+        res.status(400).send({ msg: `Error: ${error}` })
+    }
+
 }
 
 module.exports = {
